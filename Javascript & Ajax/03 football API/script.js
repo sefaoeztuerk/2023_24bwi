dda67edcc26e496a5c5ef7431838328a5e73c318
sefaoeztuerk <113421062+sefaoeztuerk@users.noreply.github.com>
2023-09-22 16:15:21 +0200

    football-api finished

---
+++ Javascript & Ajax/03 football API/script.js
@@ -0,0 +1,20 @@
+const loadData = ()=>{
+    fetch("https://api.openligadb.de/getbltable/bl1/2022").then((result)=>{
+        result.json().then((data)=>{
+            fillTable(data);
+        });
+
+    });
+};
+
+const fillTable = (data)=>{
+
+    let html="";
+
+    data.forEach(element=> {
+        html += "<div>" +   element.points + "by Team" + element.teamName + "</div>";
+    });
+    document.getElementById("output").innerHTML=html;
+}
+
+loadData();
