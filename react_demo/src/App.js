d6c34e86ed04b073b4d56d2aab8009361f05dbb5
sefaoeztuerk <113421062+sefaoeztuerk@users.noreply.github.com>
2023-12-13 12:17:24 +0100

    Tailwind eingestellt

--- react_demo/src/App.js
+++ react_demo/src/App.js
@@ -1,15 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-import Person from './components/Person';
 
 function App() {
   return (
-    <div classname="App" style={{border:"solid"}}>
-      <h1></h1>
-      <Person name="Diego" description="ist Coder"/>
-      <Person name="Maxim" description="ist Coder"/>
-      <Person name="Tamir" description="ist Coder"/>
-
+    <div className="bg-green-400">
+      <h1>Hello</h1>
     </div>  
     );
 }
