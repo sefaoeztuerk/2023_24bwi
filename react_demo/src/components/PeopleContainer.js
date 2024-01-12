bbb28b2de40b9fa0767682293fbc71cef3ed774a
sefaoeztuerk <113421062+sefaoeztuerk@users.noreply.github.com>
2024-01-12 14:45:13 +0100

    4 Demo Filter Aufgabe fertig gemacht

--- react_demo/src/components/PeopleContainer.js
+++ react_demo/src/components/PeopleContainer.js
@@ -1,30 +1,41 @@
-import React, { useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from './Card'
 
 
 export default function PeopleContainer() {
 
-
-    const [people, setPeople] = useState ([]);
-
+    const [people, setPeople] = useState([]);
+    const [filteredPeople, setFilteredPeople] = useState([])
 
     useEffect(() => {
         fetch("https://659e6f1f47ae28b0bd35d432.mockapi.io/person").then(
-            (res) => res.json().then((data) =>{
+            (res) => res.json().then((data) => {
                 setPeople(data);
-                
+                setFilteredPeople(data);
+
             })
         );
     }, []);
 
+    const filterPeople = (filter) => {
+        let filtered = people.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()));
+        setFilteredPeople(filtered);
+    }
+
 
     return (
-        <div>
-            <h1>People</h1>
-            <div className='grid grid-cols-2 lg:grid-cols-4 gap-4'>
-            {people.map(person => {
-                return <Card name={person.name} imageUri={person.avatar} title={person.jobtitle}/>
-            })}    
+        <div className="">
+            <div className="fixed border mb-4">
+                <h1>People</h1>
+                <input className="border p-2" type="text" placeholder="Search" onChange={(el) => {
+                    console.log(el.target.value);
+                    filterPeople(el.target.value);
+                }} />
+            </div>
+            <div className='grid grid-cols-2 lg:grid-cols-4 gap-4 p-8 pt-32'>
+                {filteredPeople.map(person => {
+                    return <Card name={person.name} imageUri={person.avatar} title={person.jobtitle} />
+                })}
             </div>
         </div>
     )
