14863fe3206d67b72723ddbbb586f6626ab6526f
sefaoeztuerk <113421062+sefaoeztuerk@users.noreply.github.com>
2024-02-28 11:23:20 +0100

    App js angepasst und Css erstellt

--- react_demo/src/App.js
+++ react_demo/src/App.js
@@ -1,23 +1,9 @@
 import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import People from './Router/People';
-import Friends from './Router/Friends';
 
-const router = createBrowserRouter([
-  {
-    path: "/People",
-    element: <People />,
-  },
-  {
-    path: "/Friends",
-    element: <Friends />,
-  },
-]);
-
-
-function App() {
-  return (<RouterProvider router={router} />
+const App = () => {
+  return (
+    <div className="App">
+      <Fussball />
+    </div>
   );
-}
-
-export default App;
+};
