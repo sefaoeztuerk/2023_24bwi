ac77f30308d6801f7a319d1aecc2ff01cf20cb00
sefaoeztuerk <113421062+sefaoeztuerk@users.noreply.github.com>
2024-03-18 15:04:30 +0100

    Button.js und logo.js erstellt

---
+++ react_demo/src/organismen/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Table from './Table';
+
+
+
+const Main = ({ tableData }) => {
+    return (
+        <div className="main">
+            <Table tableData={tableData} />
+        </div>
+    );
+}
+
+export default Main;
