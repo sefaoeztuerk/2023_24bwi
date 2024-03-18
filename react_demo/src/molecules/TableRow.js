ef94dfbba4d53e95166285f8b3581f6f2028c0d0
sefaoeztuerk <113421062+sefaoeztuerk@users.noreply.github.com>
2024-03-18 15:46:12 +0100

    TableRow done

--- react_demo/src/molecules/TableRow.js
+++ react_demo/src/molecules/TableRow.js
@@ -1,2 +1,36 @@
+import React, { useEffect } from 'react';
 
+const TableRow = ({ data, index }) => {
+    return (
+        <div key={index} className="tableRow">
+            <div className="leftPart">
+                <div className="placement">{index + 1}</div>
+                <div className="clubLogo">
+                    {data && data.teamIconUrl && (
+                        <img src={data.teamIconUrl} alt={data.teamName} />
+                    )}
+                </div>
+                <div className="clubName">{data && data.teamName}</div>
+            </div>
+            <div className="middlePart">
+                {data && (
+                    <>
+                        <div className="matchPlayed">{data.matches}</div>
+                        <div className="win">{data.won}</div>
+                        <div className="draw">{data.draw}</div>
+                        <div className="lose">{data.lost}</div>
+                        <div className="goalFor">{data.goals}</div>
+                        <div className="goalAgainst">{data.opponentGoals}</div>
+                        <div className="goalDif">{data.goalDiff}</div>
+                    </>
+                )}
+            </div>
+            <div className="rightPart">
+                {data && <div className="points">{data.points}</div>}
+            </div>
+        </div>
+    );
+}
+
+export default TableRow;
 
