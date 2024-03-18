56908fef0c51d59a2736224b6b4410596f6d4597
sefaoeztuerk <113421062+sefaoeztuerk@users.noreply.github.com>
2024-03-18 15:20:45 +0100

    Header und Tableheader erstellt

--- react_demo/src/molecules/Header.js
+++ react_demo/src/molecules/Header.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import Logo from '../atoms/Logo';
 
+const Header = () => {
+    return (
+        <header className="header">
+            <Logo />
+            <div className="headline">Season 2023/24</div>
+        </header>
+    );
+}
+
+export default Header;
--- react_demo/src/molecules/TableHeader.js
+++ react_demo/src/molecules/TableHeader.js
@@ -1,1 +1,25 @@
+import React from 'react';
 
+const TableHeader = () => {
+    return (
+        <div className="tableHeader">
+            <div className="leftPart">
+                <div className="club">Club</div>
+            </div>
+            <div className="middlePart">
+                <div className="matchPlayed">mp</div>
+                <div className="win">w</div>
+                <div className="draw">d</div>
+                <div className="lose">l</div>
+                <div className="goalFor">gf</div>
+                <div className="goalAgainst">ga</div>
+                <div className="goalDif">gd</div>
+            </div>
+            <div className="rightPart">
+                <div className="points">Pts</div>
+            </div>
+        </div>
+    );
+}
+
+export default TableHeader;
