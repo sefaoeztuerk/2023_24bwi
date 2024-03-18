ca4989226be9a8074cd78fc82b1046eb29272712
sefaoeztuerk <113421062+sefaoeztuerk@users.noreply.github.com>
2024-03-18 16:09:26 +0100

    Table.js done

--- react_demo/src/organismen/Table.js
+++ react_demo/src/organismen/Table.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import TableHeader from '../molecules/TableHeader';
+import TableRow from '../molecules/TableRow';
 
+const Table = ({ tableData }) => {
+    return (
+        <div className="table">
+            <TableHeader />
+            {tableData.map((data, index) => (
+                <TableRow key={index} data={data} index={index} />
+            ))}
+        </div>
+    );
+}
+
+export default Table;
