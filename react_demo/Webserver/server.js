1f60ab4db8baadfeaada84c777967035d300325c
sefaoeztuerk <113421062+sefaoeztuerk@users.noreply.github.com>
2024-04-26 14:35:51 +0200

    node express post done

--- react_demo/Webserver/server.js
+++ react_demo/Webserver/server.js
@@ -1,22 +1,27 @@
 const express = require('express')
-const app = express()
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extendet: false }));
+app.use(bodyParser.json());
+
 
 const data = [
 
     { name: "Maxe", age: 17 },
     { name: "Diego", age: 17 },
     { name: "Tamir", age: 17 },
-]
+];
 
-
-
-app.get('/people', function (req, res) {
+app.get('/people', (req, res) => {
     res.send(data)
-})
+});
 
-app.put('/people', function (req, res) {
-    console.log(req, body)
-})
+app.post('/people', (req, res) => {
+    data.push(req.body);
+    res.send(req.body);
+});
 
 app.listen(3001, function () {
     console.log('Server läuft auf port 3001');
